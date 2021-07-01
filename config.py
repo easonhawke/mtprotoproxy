b6818d566302a25b76fa437501ca8d4f8b5a8b92
Eason <easonhawke@sina.com>
2021-07-01 10:30:50 +0800

    Add files via upload
    我的配置文件

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "6aba808729630748572148be53299109",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -12,7 +12,7 @@ MODES = {
 
     # Makes the proxy harder to detect
     # Can be incompatible with very old clients
-    "secure": False,
+    "secure": True,
 
     # Makes the proxy even more hard to detect
     # Can be incompatible with old clients
@@ -21,7 +21,10 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.cloudflare.com"
 
 # Tag for advertising, obtainable from @MTProxybot
 # AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+
+TO_CLT_BUFSIZE = 262144
+TO_TG_BUFSIZE = 262144
